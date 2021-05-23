c52fdcc322d8b9ae24265df4aff7cc769a9c7d41
seca00 <sercanakca94@gmail.com>
2021-05-23 19:14:32 +0300

    Made improvements, by adding index on NR.datetime

--- navigation/models.py
+++ navigation/models.py
@@ -9,3 +9,13 @@ class NavigationRecord(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
 
+    # Improvement!!
+    # Some databases already does something for datetime indexing but for improvement, 
+    # it would help to index datetime as it would make searching by datetime faster.
+    # As far as I know, some databases only does indexing by date which is more than 10x faster
+    # than datetime since it doesn't count for time and miliseconds
+    # Fore more information: https://stackoverflow.com/questions/17381875/how-to-improve-performance-for-datetime-filtering-in-sql-server
+    class Meta:
+        indexes = [
+            models.Index(fields=['datetime'], name='datetime_idx'),
+        ]
